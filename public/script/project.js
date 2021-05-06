703771c777cc0b626cbbd7d01d2fbf01769814ce
Arthur-MONNET <artmon2002@gmail.com>
2021-05-06 23:55:34 +0200

    comment style + add arrow in project

--- public/script/project.js
+++ public/script/project.js
@@ -6,16 +6,24 @@ const text2 = document.querySelector("#text2 p")
 const image2 = document.querySelector("#img2")
 let params = (new URL(document.location)).searchParams;
 let projectIndex = parseInt(params.get('project'))
-
+let projectIndexN = projectIndex-1
+let projectIndexP = projectIndex+1
 window.addEventListener("load", () => {
 
     setTimeout(()=>{
-
+        if (projectIndexN===0){
+            projectIndexN = nbProject
+        }
+        if (projectIndexP>nbProject){
+            projectIndexP = 1
+        }
         titleProject.innerHTML = listProject[projectIndex].title
         image1.src = listProject[projectIndex].img1
         text1.innerHTML= listProject[projectIndex].desc1
         image2.src = listProject[projectIndex].img2
         text2.innerHTML= listProject[projectIndex].desc2
+        arrowLeft.href = `./project.html?project=${projectIndexN}`
+        arrowRight.href = `./project.html?project=${projectIndexP}`
         setTimeout(()=>{
         image2.style= `height : ${image2.offsetHeight*7/9}px`
         document.querySelector("body").style= `height: calc(95vh + 70vh + ${image2.offsetHeight}px)`
